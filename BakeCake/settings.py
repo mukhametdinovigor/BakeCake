12f44a75b0608ce4000b8eca0f178b787b752ee9
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-10-26 22:03:26 +1100

    Add template

--- BakeCake/settings.py
+++ BakeCake/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,8 +116,9 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'assets'),
+    ]
 
 STATIC_URL = '/static/'
 
