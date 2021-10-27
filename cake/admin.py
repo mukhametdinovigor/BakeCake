eda6a46ce6243c5cdb628b658d4f941312e2ee7d
multipassport <mashross@gmail.com>
2021-10-27 19:27:53 +0300

    Add simple operable models to admin page

--- cake/admin.py
+++ cake/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Level, Shape, Topping, Berry, AdditionalIngredient, Cake, Customer, Order
+)
+
+
+@admin.register(Level)
+class LevelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Shape)
+class ShapeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Topping)
+class ToppingAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Berry)
+class BerryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AdditionalIngredient)
+class AdditionalIngredientAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Cake)
+class CakeAdmin(admin.ModelAdmin):
+    # inlines = [
+    #     LevelInline,
+    #     ShapeInline,
+    #     ToppingInline,
+    #     BerryInline,
+    #     AdditionalIngredientInline,
+    # ]
+    pass
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
