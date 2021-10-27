3eece47b6c7ea9e41096d28be2463083d26ab9e7
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-10-28 06:46:12 +1100

    Simplify creating choices for cake elements

--- cake/forms.py
+++ cake/forms.py
@@ -1,52 +1,32 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
-from .models import Customer
+from .models import Customer, Level, Shape, Topping, Berry, AdditionalIngredient
+
+
+def create_elements():
+    models = (Level, Shape, Topping, Berry, AdditionalIngredient)
+    elements = dict()
+    for model in models:
+        choices = model.objects.values_list()
+        elements[model] = choices
+    return elements
+
+
+def create_choices(elements, model):
+    choices = []
+    for choice in elements[model]:
+        choices.append((f'{choice[2]}', f'{choice[1]} + {choice[2]} руб.'))
+    return choices
 
 
 class ConstructCakeForm(forms.Form):
-    LEVEL_CHOICES = [
-        ('level1', '1 уровень'),
-        ('level2', '2 уровня'),
-        ('level3', '3 уровня')
-    ]
-    LEVEL_SHAPES = [
-        ('circle', 'Круг'),
-        ('square', 'Квадрат'),
-        ('rectangle', 'Прямоугольник')
-    ]
-
-    TOPPINGS = [
-        ('without_toppings', 'Без топпинга'),
-        ('white_souse', 'Белый соус'),
-        ('caramel_syrup', 'Карамельный сироп'),
-        ('maple_syrup', 'Кленовый сироп'),
-        ('strawberry_syrup', 'Клубничный сироп'),
-        ('blueberry_syrup', 'Черничный сироп'),
-        ('milk_choco', 'Молочный шоколад')
-    ]
-    BERRIES = [
-        ('blackberry', 'Ежевика'),
-        ('raspberry', 'Малина'),
-        ('blueberry', 'Голубика'),
-        ('Strawberry', 'Клубника')
-    ]
-
-    DECOR = [
-        ('without_decor', 'Без декора'),
-        ('pistachio', 'Фисташки'),
-        ('meringue', 'Безе'),
-        ('funduk', 'Фундук'),
-        ('pecan', 'Пекан'),
-        ('Marshmallow', 'Маршмеллоу'),
-        ('marzipan', 'Марципан'),
-    ]
-
-    levels = forms.ChoiceField(label='Количество уровней', choices=LEVEL_CHOICES, widget=forms.RadioSelect)
-    level_shapes = forms.ChoiceField(label='Форма уровней', choices=LEVEL_SHAPES, widget=forms.RadioSelect)
-    toppings = forms.ChoiceField(label='Топпинг', choices=TOPPINGS, widget=forms.RadioSelect)
-    berries = forms.MultipleChoiceField(label='Ягоды', choices=BERRIES, widget=forms.CheckboxSelectMultiple)
-    decor = forms.MultipleChoiceField(label='Декор', choices=DECOR, widget=forms.CheckboxSelectMultiple)
+    elements = create_elements()
+    levels = forms.ChoiceField(label='Форма уровней', choices=create_choices(elements, Level), widget=forms.RadioSelect)
+    shapes = forms.ChoiceField(label='Форма уровней', choices=create_choices(elements, Shape), widget=forms.RadioSelect)
+    toppings = forms.ChoiceField(label='Топпинг', choices=create_choices(elements, Topping), widget=forms.RadioSelect)
+    berries = forms.MultipleChoiceField(label='Ягоды', choices=create_choices(elements, Berry), widget=forms.CheckboxSelectMultiple)
+    decor = forms.MultipleChoiceField(label='Декор', choices=create_choices(elements, AdditionalIngredient), widget=forms.CheckboxSelectMultiple)
     lettering = forms.CharField(label='Мы можем разместить на торте любую надпись, например: «С днем рождения!»', max_length=500)
 
 
